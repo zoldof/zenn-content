34c49af6f6befb4cefc66cc62cb2081d9b4bf67a
zoldof <zoldof.game@gmail.com>
2025-05-21 17:26:14 +0900

    Update inject_snippet.py

--- scripts/inject_snippet.py
+++ scripts/inject_snippet.py
@@ -10,10 +10,9 @@ with snippet_path.open() as f:
     code = f.read()
 
 # 置換
-new_content = content.replace(
-    "```python\n<!-- snippet:greet.py -->\n```",
-    f"```python\n{code}\n```"
-)
+pattern = r'```python\n.*?\n```'
+replacement = f'```python\n{code}\n```'
+new_content = re.sub(pattern, replacement, content, flags=re.DOTALL)
 
 with md_path.open("w") as f:
     f.write(new_content)
