b41a478e6138801a2a20cf382116d4ba05ed8576
zoldof <zoldof.game@gmail.com>
2025-06-09 12:26:03 +0900

    Update init.py

--- tools/init.py
+++ tools/init.py
@@ -1,16 +1,18 @@
 #モジュール
-import time
-import tracemalloc
-import random
+##初期データ用
+###import random
+
+##測定用
+import time, tracemalloc
+
+##各種アルゴリズム共通簡略化
+#from datetime import timedelta
+
+##出力用
 import json
-#from datetime import datetime, timedelta
-#from dateutil import parser
-#import pytz
-#import pandas as pd
-##import psutil, os, time
 
 #データ作成
-n = 1000
-min_val = 0
-max_val = 999
-data = [random.randint(min_val, max_val) for _ in range(n)]
+###n = 1000
+###min_val = 0
+###max_val = 999
+###data = [random.randint(min_val, max_val) for _ in range(n)]
