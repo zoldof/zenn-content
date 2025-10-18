7276945d1b546cb77f25251d62a91210249d8f16
zoldof <zoldof.game@gmail.com>
2025-10-19 07:14:34 +0900

    Update mearge.py

--- tools/mearge.py
+++ tools/mearge.py
@@ -1,1 +1,22 @@
+#**********
+#init.py
+#**********
 
+#モジュール
+##初期データ用
+###import random
+
+##測定用
+import time, tracemalloc
+
+##各種アルゴリズム共通簡略化
+###from datetime import timedelta
+
+##出力用
+import json
+
+#データ作成
+###n = 1000
+###min_val = 0
+###max_val = 999
+###data = [random.randint(min_val, max_val) for _ in range(n)]
